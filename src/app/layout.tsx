2fd8fb249d4ec86e4117ff0feabdf1b7ba6c865f
tonypham <tunglamldk@gmail.com>
2023-09-07 11:07:52 +0700

    Add top loading page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.scss';
 import type { Metadata } from 'next';
 import { Fira_Sans } from 'next/font/google';
+import NextTopLoader from 'nextjs-toploader';
 
 const firaSans = Fira_Sans({
   weight: '400',
@@ -18,7 +19,20 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={`sm:invisible lg:visible ${firaSans.className}`}>{children}</body>
+      <body className={`sm:invisible lg:visible ${firaSans.className}`}>
+        <NextTopLoader
+          color="#2299DD"
+          initialPosition={0.08}
+          crawlSpeed={200}
+          height={10}
+          crawl={true}
+          showSpinner={true}
+          easing="ease"
+          speed={200}
+          shadow="0 0 10px #2299DD,0 0 5px #2299DD"
+        />
+        {children}
+      </body>
     </html>
   );
 }
